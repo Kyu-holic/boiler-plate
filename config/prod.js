1b24234afb85b71aeb9aee6ea60dda3f73399879
unknown <khhan1990@hanmail.net>
2021-09-08 21:13:26 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI, // 나중에 HEROKU 사용시 MONGO_URI 부분은 이름을 똑같이 해줘야 한다.
+};
--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        maxlength: 50
+        maxlength: 50,
     },
     email: {
         type: String,
@@ -26,12 +25,12 @@ const userSchema = mongoose.Schema({
     image: String,
     token: {
         type: String,
-    }
+    },
     tokenExp: {
         type: Number,
-    }
-})
+    },
+});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.export = {User}
+module.exports = { User };
